99621187c197e7243d4de4dda137dee2c4aaac8d
faketooth <faketooh@qq.com>
2015-11-04 00:07:27 +0800

    modified:   _src/om2py3w/3wex0/client.py
    client enhencement:
    1. help message
    2. ask username
    3. block empty message at client
    4. prepare for history diary

--- _src/om2py3w/3wex0/client.py
+++ _src/om2py3w/3wex0/client.py
@@ -4,13 +4,33 @@ import socket
 
 def main():
 	clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	welcome = 'Welcome to use this net-diary. Please input your name:\n>>> '
+	username = raw_input(welcome)
+	while not username:
+		username = raw_input(welcome)
+	print "Hello, %s! " % (username)
+	print "q/quit/exit for quit."
+	print "history/r for history diary."
+	print "hit Enter-key to send the message."
 	while True:
 		input = raw_input(">>> ")
 		if input in ['q', 'quit', 'exit']:
+			quit(clientSock)
 			break
+		if input in ['']:
+			print "Empty Message won't send!"
+			continue
+		if input in ['history', 'r']:
+			history()
+			continue
 		clientSock.sendto(input, ('localhost', 9527))
-		
+	print "Bye~~"
+
+def quit(clientSock):		
 	clientSock.close()
+	
+def history():
+	print "Okey. Here is your history diary..."
 	
 if __name__ == '__main__':
 	main()
